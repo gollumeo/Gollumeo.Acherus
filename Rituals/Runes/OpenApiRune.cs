832f0fb0b04a1047e8459ba36ffe761e3fb179f9
Golluméo <p.mauriello54@gmail.com>
2025-06-10 19:10:09 +0200

    feat(runes): add OpenApiRune to enable OpenAPI support
    Introduce a new `OpenApiRune` class implementing `IRune`. This enables modular configuration of OpenAPI support, adding services in the DI container and mapping OpenAPI endpoints during development environments.

---
+++ Rituals/Runes/OpenApiRune.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Rituals.Contracts;
+using Rituals.Extensions;
+
+namespace Rituals.Runes;
+
+public class OpenApiRune : IRune
+{
+    public void Extract(IServiceCollection services, IConfiguration config)
+    {
+        services.AddOpenApi();
+    }
+
+    public void CarveWith(WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+            app.MapOpenApi();
+    }
+}
