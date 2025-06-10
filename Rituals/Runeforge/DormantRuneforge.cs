037f3dabae2823430b189e9ec292885cbaba9a40
Golluméo <p.mauriello54@gmail.com>
2025-06-10 19:11:20 +0200

    feat(runeforge): add abstract DormantRuneforge class
    Introduce `DormantRuneforge` abstract class to enable modular application bootstrapping. It leverages runes for dynamic service registration and app setup. Provides a template method `Inscribe` and requires derived classes to implement `Frostmourne`.

---
+++ Rituals/Runeforge/DormantRuneforge.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Rituals.Contracts;
+using Rituals.Runes;
+
+namespace Rituals.Runeforge;
+
+public abstract class DormantRuneforge
+{
+    private static readonly IRune[] RunesOfTheFallenCrusader =
+    [
+        new HttpRune(),
+        new OpenApiRune()
+    ];
+    
+    public async Task Inscribe(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var runes = RunesOfTheFallenCrusader.Concat(Frostmourne()).ToList();
+
+        Console.WriteLine("🪄 Activating runes:");
+        foreach (var rune in runes)
+        {
+            rune.Extract(builder.Services, builder.Configuration);
+            Console.WriteLine($" → {rune.GetType().Name}");
+        }
+
+        var app = builder.Build();
+
+        foreach (var rune in runes)
+            rune.CarveWith(app);
+
+        await app.RunAsync();
+    }
+
+    protected abstract IEnumerable<IRune> Frostmourne();
+}
