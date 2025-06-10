eae3e09556cdaf862899928463c8cb5b85b0748f
Golluméo <p.mauriello54@gmail.com>
2025-06-10 19:10:48 +0200

    feat(runes): add IRune interface for modular configuration
    Introduce the `IRune` interface in the Runes namespace to define a contract for modular configuration. Provides methods `Extract` for service registration and `CarveWith` for app customization, enabling flexibility and consistency in the extension design.

---
+++ Rituals/Contracts/IRune.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Rituals.Contracts;
+
+public interface IRune
+{
+    void Extract(IServiceCollection services, IConfiguration config);
+    void CarveWith(WebApplication app);
+}
