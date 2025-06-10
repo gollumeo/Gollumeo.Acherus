d2bc9fd3e6664e55ccec5e8459861984b8331d79
Golluméo <p.mauriello54@gmail.com>
2025-06-10 19:11:06 +0200

    feat(runes): add HttpRune for HTTP support
    Introduce `HttpRune` class implementing `IRune` to enable HTTP-related functionalities. Adds controllers to the service container and maps them within the app, ensuring HTTP endpoints and authorization are properly configured.

---
+++ Rituals/Runes/HttpRune.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Rituals.Contracts;
+
+namespace Rituals.Runes;
+
+public class HttpRune : IRune
+{
+    public void Extract(IServiceCollection services, IConfiguration config)
+    {
+        services.AddControllers();
+    }
+
+    public void CarveWith(WebApplication app)
+    {
+        app.MapControllers();
+        app.UseAuthorization();
+    }
+}
