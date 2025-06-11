86d0d54aefce5b083edc65fa56e510b2c593f3d2
Golluméo <p.mauriello54@gmail.com>
2025-06-11 14:49:18 +0200

    feat(application): add Result class for encapsulating operation outcomes
    Introduce `Result` and `Result<T>` classes to encapsulate operation outcomes consistently. These classes provide methods for handling success and failure cases, enhancing code readability and error handling throughout the application.

---
+++ Application/Result.cs
@@ -0,0 +1,32 @@
+﻿namespace Application;
+
+public class Result<T>
+{
+    public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
+    public T Value { get; }
+    public string Error { get; }
+
+    public Result(bool isSuccess, string error, T value)
+    {
+        IsSuccess = isSuccess;
+        Error = error;
+        Value = value;
+    }
+    
+    public static Result<T> Success(T value) => new(true, string.Empty, value);
+    public static Result<T> Failure(string error) => new(false, error, default!);
+    
+    public TResult Match<TResult>(Func<T, TResult> onSuccess, Func<string, TResult> onFailure)
+        => IsSuccess ? onSuccess(Value!) : onFailure(Error);
+}
+
+
+public class Result : Result<object>
+{
+    private Result(bool isSuccess, string error)
+        : base(isSuccess, error, new object()) { }
+
+    public static Result Success() => new(true, string.Empty);
+    public new static Result Failure(string error) => new(false, error);
+}
